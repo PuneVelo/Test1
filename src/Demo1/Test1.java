8fe0fb00806cfa4855e854ac174264aee77a0c89
admin <admin@LAPTOP-DBKJ4JPG>
2023-01-23 21:12:11 +0530

    commit

---
+++ src/Demo1/Test1.java
@@ -0,0 +1,25 @@
+package Demo1;
+
+public class Test1 {
+	
+	public static void main(String[] args)
+	{
+		//* * * *
+	    //* * * *
+		//* * * *
+	    //* * * *
+		
+		//For Row
+		for(int i=1;i<=4;i++)
+		{
+			//For coloumn
+			for(int a=1;a<=4;a++)
+			{
+				System.out.print("*"+" " );
+			}
+			System.out.println();
+		}
+	}
+
+
+}
